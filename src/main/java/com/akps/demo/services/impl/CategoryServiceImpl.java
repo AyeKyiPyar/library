3024270d70860c1bd86b406df651ada46ae711d6
KyiPyar <ayekyipyarshwe@gmail.com>
2026-03-23 15:20:07 +0630

    updated

--- src/main/java/com/akps/demo/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/akps/demo/services/impl/CategoryServiceImpl.java
@@ -5,7 +5,7 @@ package com.akps.demo.services.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.akps.demo.mapper.CategoryMapper;
-import com.akps.demo.mapper.CategoryMapper;
+
 import com.akps.demo.repositories.CategoryRepository;
 import com.akps.demo.requests.CreateCategoryRequest;
 import com.akps.demo.responses.CategoryResponse;
@@ -14,16 +14,9 @@ import com.akps.demo.services.CategoryService;
 @Service
 public class CategoryServiceImpl implements CategoryService
 {
-    private final CategoryMapper categoryMapper;
-	
 	@Autowired
 	private CategoryRepository categoryRepository;
-    CategoryServiceImpl(CategoryMapper categoryMapper) 
-    {
-        this.categoryMapper = categoryMapper;
-    }
-
-
+	
 	@Override
 	public CategoryResponse save(CreateCategoryRequest request)
 	{
