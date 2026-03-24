ccc5143e9ae0d6a9236a6708e0e4e2b551196902
KyiPyar <ayekyipyarshwe@gmail.com>
2026-03-24 11:37:10 +0630

    updated categoryrepositoryimpl

--- src/main/java/com/akps/demo/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/akps/demo/services/impl/CategoryServiceImpl.java
@@ -1,3 +1,6 @@
+/**
+ * CategoryService Implementation
+ */
 package com.akps.demo.services.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,12 +10,18 @@ import com.akps.demo.requests.CreateCategoryRequest;
 import com.akps.demo.responses.CategoryResponse;
 import com.akps.demo.services.CategoryService;
 
+/**
+ * Implemented by AKPS at March 2026
+ */
 @Service
 public class CategoryServiceImpl implements CategoryService{
 	
+	// declare and auto create category repository
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	
+	// category save method
 	@Override
 	public CategoryResponse save(CreateCategoryRequest request){
 		
